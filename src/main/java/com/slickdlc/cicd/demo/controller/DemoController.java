49a255848594ea9238ebcf639ed5228a9c016f57
Hans De La Cruz Acosta <48918327+slickdlc@users.noreply.github.com>
2025-09-02 17:30:58 -0500

    Feature/ci cd workflow (#1)
    * initial commit
    
    Co-authored-by: Hans De La Cruz <hansdelacruzacosta@gmail.com>

---
+++ src/main/java/com/slickdlc/cicd/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.slickdlc.cicd.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+public class DemoController {
+
+  @PostMapping("/hello")
+  public ResponseEntity<String> hello() {
+    return ResponseEntity.ok("Hello, World!");
+  }
+
+}
